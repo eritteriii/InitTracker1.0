d21e22068687123ad6f19e184cdb8cf976e071cb
steventwig <steventwig@gmail.com>
2019-11-26 14:41:39 -0800

    make it better

--- inittracker1.0/client/src/components/Chat/Chat.js
+++ inittracker1.0/client/src/components/Chat/Chat.js
@@ -49,7 +49,7 @@ const Chat = (props) => {
 	const [users, setUsers] = useState('');
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
-	const ENDPOINT = 'http://localhost:5000/';
+	const ENDPOINT = 'https://inittracker1-0.herokuapp.com/';
 	const classes = useStyles();
 	const [anchorEl, setAnchorEl] = React.useState(null);
 	const open = Boolean(anchorEl);
